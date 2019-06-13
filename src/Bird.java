ea1bb759da55fd6440d151b5f24ea4a4db64dc5b
dravin_nagalingam <dravin_nagalingam@student.wayland.k12.ma.us>
2019-06-13 18:40:26 -0400

    upload 2

--- src/Bird.java
+++ src/Bird.java
@@ -3,14 +3,12 @@ import java.awt.*;
 public class Bird extends Sprite{
 
     public Bird(){
-        super(400,400,SOUTH);
+        super(75,250,SOUTH);
         setPic("bird.png",SOUTH);
-        setSpeed(20);
+        setSpeed(4);
     }
 
-    public boolean intersects(Sprite other) {
-        return getBoundingRectangle().intersects(other.getBoundingRectangle());
-    }
+
 
     @Override
     public void update(){
--- src/Main.java
+++ src/Main.java
@@ -3,70 +3,91 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.ArrayList;
 
 public class Main extends JPanel {
 
+    //general environment
     public static final int FRAMEWIDTH = 1400, FRAMEHEIGHT = 600;
     private Timer timer;
+    private int speed = 10;
+    private boolean[] keys;
+
 
     //buffered images
-    private BufferedImage bird;
+    private Bird bird;
     private BufferedImage ground;
     private BufferedImage city;
 
+
     private ArrayList<Sprite> pipes;
 
-    public Main(){
+    public Main() {
+        keys = new boolean[512];
 
         try {
-            bird = ImageIO.read(new File("res/" + "bird.png"));
             ground = ImageIO.read(new File("res/" + "ground.png"));
             city = ImageIO.read(new File("res/" + "citybackground.png"));
-        }catch (Exception e){e.printStackTrace();}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
+        bird = new Bird();
 
         pipes = new ArrayList();
         //pair 1
-        pipes.add(new Pipe(1100,350,Sprite.WEST,0));
-        pipes.add(new Pipe(1100,0,Sprite.WEST,1));
+        pipes.add(new Pipe(1100, 350, Sprite.WEST, 0));
+        pipes.add(new Pipe(1100, 0, Sprite.WEST, 1));
         //pair 2
-        pipes.add(new Pipe(1400,200,Sprite.WEST,2));
-        pipes.add(new Pipe(1400,0,Sprite.WEST,3));
+        pipes.add(new Pipe(1400, 200, Sprite.WEST, 2));
+        pipes.add(new Pipe(1400, 0, Sprite.WEST, 3));
         //pair 3
-        pipes.add(new Pipe(800,300,Sprite.WEST,4));
-        pipes.add(new Pipe(800,0,Sprite.WEST,5));
+        pipes.add(new Pipe(800, 300, Sprite.WEST, 4));
+        pipes.add(new Pipe(800, 0, Sprite.WEST, 5));
         //pair 4
-        pipes.add(new Pipe(425,350,Sprite.WEST,0));
-        pipes.add(new Pipe(425,0,Sprite.WEST,1));
+        pipes.add(new Pipe(425, 350, Sprite.WEST, 0));
+        pipes.add(new Pipe(425, 0, Sprite.WEST, 1));
 
 
         timer = new Timer(40, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
 
-//                boolean dead = false;
-//                for(Sprite pipe : pipes){
-//                   if(bird.intersects(pipe)){
-//                       dead = true;
-//                       timer.stop();
-//                   }
-//                }
+                boolean dead = false;
+                for(Sprite pipe : pipes){ //if the bird hits a pipe it dies
+                   if(bird.intersects(pipe)){
+                       dead = true;
+                       timer.stop();
+                   }
+                }
+
+                //if a bird hits the ground it dies
+                if(bird.getLoc().y>=470) {
+                    dead = true;
+                    timer.stop();
+                }
 
 
-                for(Sprite pipe: pipes)
+                //bird is constantly falling
+                bird.getLoc().y += speed;
+
+                for (Sprite pipe : pipes)
                     pipe.update();
 
 
-                repaint(); //always last line!!
+                moveBird();
+
+                repaint(); //keep as last line
             }
         });
         timer.start();
 
+        setKeyListener();
     }
-
 
 
     public void paintComponent(Graphics g) {
@@ -74,30 +95,60 @@ public class Main extends JPanel {
         Graphics2D g2 = (Graphics2D) g;
 
         //drawing the background
-        g2.drawImage(ground,0,500,null);
-        g2.drawImage(city,0,0,null);
+        g2.drawImage(ground, 0, 500, null);
+        g2.drawImage(city, 0, 0, null);
 
         //drawing the pipes
-        for(Sprite pipe : pipes)
+        for (Sprite pipe : pipes)
             pipe.draw(g2);
 
         //drawing the score
         g2.setColor(Color.white);
         g.setFont(new Font("TimesRoman", Font.PLAIN, 25));
-        g2.drawString("SCORE: ",35,50);
+        g2.drawString("SCORE: ", 35, 50);
 
         //drawing the bird
-        g2.drawImage(bird,75,250,null);
+        bird.draw(g2);
 
+    }
+
+    public void moveBird() {
+        if (keys[KeyEvent.VK_SPACE] || keys[KeyEvent.VK_UP]) {
+            bird.setDir(Sprite.SOUTH);
+            bird.getLoc().y -= speed;
+            bird.update();
+            keys[KeyEvent.VK_SPACE] = false;
+            keys[KeyEvent.VK_UP] = false;
+        }
+
+    }
+
+    public void setKeyListener() {
+        addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent keyEvent) {/*intentionally left blank*/ }
+
+            //when a key is pressed, its boolean is switch to true.
+            @Override
+            public void keyPressed(KeyEvent keyEvent) {
+                keys[keyEvent.getKeyCode()] = true;
+            }
+
+            //when a key is released, its boolean is switched to false.
+            @Override
+            public void keyReleased(KeyEvent keyEvent) {
+                keys[keyEvent.getKeyCode()] = false;
+            }
+        });
     }
 
     public static void main(String[] args) {
         JFrame window = new JFrame("Flappy Bird!!");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setBounds(0,0,FRAMEWIDTH,FRAMEHEIGHT+22);
+        window.setBounds(0, 0, FRAMEWIDTH, FRAMEHEIGHT + 22);
         window.setBackground(Color.cyan);
         Main panel = new Main();
-        panel.setSize(1000,600);
+        panel.setSize(FRAMEWIDTH, FRAMEHEIGHT);
 
         panel.setFocusable(true);
         panel.grabFocus();
@@ -105,5 +156,4 @@ public class Main extends JPanel {
         window.setVisible(true);
         window.setResizable(false);
     }
-
 }
